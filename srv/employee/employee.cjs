4a816ca717bfa6d5952da1ca2690d0c1044cedd7
andres alvarez <andresalvarezsilva6@gmail.com>
2024-06-10 15:52:22 -0400

    feat:added new validations for the employee annotation

--- srv/employee/employee.cjs
+++ srv/employee/employee.cjs
@@ -7,6 +7,13 @@ const emailRegex =
 const validateEmail = (email) => {
   return emailRegex.test(email);
 };
+
+const SCNRegex = /^[0-9]{3}-[0-9]{2}-[0-9]{4}$/;
+
+const validateSCN = (scn) => {
+  return SCNRegex.test(scn);
+};
+
 module.exports = (srv) => {
   srv.before(["CREATE", "UPDATE"], "testService.employee", async (req) => {
     //console.log("employee struct: ", req.data);
@@ -14,28 +21,43 @@ module.exports = (srv) => {
     const bundle = textBundle.getTextBundle(locale);
     const employee = req.data;
     const employeeEmail = validateEmail(employee.email);
-    console.log(employeeEmail);
+    const employeeSCN = validateSCN(employee.socialSecurityNumber);
+    //console.log(employeeEmail);
     if (employeeEmail === false) {
       return req.error(400, bundle.getText("error1"));
     }
-
-    const existingEmail = await cds.transaction(req).run(
+    if (employeeSCN === false) {
+      return req.error(400, bundle.getText("error3"));
+    }
+    const employeeOldInfo = await cds.transaction(req).run(
       SELECT("testService.employee").where({
-        email: employee.email,
+        ID: employee.ID,
       })
     );
-    if (existingEmail.length > 1) {
-      return req.error(400, "The email is already in use");
-    }
-    const employeeSCN = employee.socialSecurityNumber;
-    if (employeeSCN !== undefined) {
-      const existingSCN = await cds.transaction(req).run(
+    //console.log(employeeOldInfo[0].email);
+    console.log(employee.email);
+    console.log(employeeOldInfo[0].email !== employee.email);
+    if (employeeOldInfo[0] && employeeOldInfo[0].email !== employee.email) {
+      const existingEmail = await cds.transaction(req).run(
         SELECT("testService.employee").where({
-          socialSecurityNumber: employeeSCN,
+          email: employee.email,
         })
       );
-      if (existingSCN.Length > 1) {
-        return req.error(400, "The social security number is already in use");
+      if (existingEmail.length) {
+        return req.error(400, bundle.getText("error2"));
+      }
+    }
+    if (
+      employeeOldInfo[0] &&
+      employeeOldInfo[0].socialSecurityNumber !== employee.socialSecurityNumber
+    ) {
+      const existingSCN = await cds.transaction(req).run(
+        SELECT("testService.employee").where({
+          socialSecurityNumber: employee.socialSecurityNumber,
+        })
+      );
+      if (existingSCN.length) {
+        return req.error(400, bundle.getText("error4"));
       }
     }
   });
