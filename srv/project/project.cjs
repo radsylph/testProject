6fa67588b64bcdab2bb22b0d40257c1d9eca045b
andres alvarez <andresalvarezsilva6@gmail.com>
2024-06-11 17:19:13 -0400

    feat:added functionality

--- srv/project/project.cjs
+++ srv/project/project.cjs
@@ -2,18 +2,14 @@ const cds = require("@sap/cds");
 const textBundle = require("../../helpers/textBundle");
 const projectHandler = require("./projectHandler");
 
-const handler = new projectHandler();
-
 console.log("project handler loaded");
 
 module.exports = (srv) => {
-  srv.before(["UPDATE", "CREATE"], "testService.project", async (req) => {
+  const { project, objective } = srv.entities;
+  const handler = new projectHandler(project, objective);
+  srv.before(["UPDATE", "CREATE"], project, async (req) => {
     const locale = req.locale;
     const bundle = textBundle.getTextBundle(locale);
     await handler.validateProject(req, bundle);
-    //const bundle = textBundle.getTextBundle(locale);
-    // const project = req.data;
-
-    // console.log("project struct: ", project);
   });
 };
