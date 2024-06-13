2fd70f5ccab9f653dc9224b0e450a563fa862c85
andres alvarez <andresalvarezsilva6@gmail.com>
2024-06-13 17:27:39 -0400

    feat:added extra validations

--- srv/project/project.cjs
+++ srv/project/project.cjs
@@ -11,5 +11,22 @@ module.exports = (srv) => {
     const locale = req.locale;
     const bundle = textBundle.getTextBundle(locale);
     await handler.validateProject(req, bundle);
+    //await handler.validateObjective(req, bundle);
+  });
+
+  srv.after("READ", project, async (req
+    
+  ) => {
+    const locale = req.locale;
+    const bundle = textBundle.getTextBundle(locale);
+    console.log("test after READ:   ", req.data);
+
+    // console.log(projectData.progress);
+    // console.log(projectData.status);
+
+    // if (projectData.progress === 100) {
+    //   projectData.status = "completed";
+    //   req.error(400, "complete idt");
+    // }
   });
 };
